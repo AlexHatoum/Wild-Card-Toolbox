5f44f890ca60ec8962ca57fb0bf4d9e151385ea3
AlexHatoum <alexander.hatoum@colorado.edu>
2019-11-19 08:35:03 -0600

    Update Genetic_Brain_Map_Tutorial_PartD.m

--- Genetic_Brain_Map_Tutorial_PartD.m
+++ Genetic_Brain_Map_Tutorial_PartD.m
@@ -1,10 +1,11 @@
-%Finally! Here we go to a matlab script. This script will take those asc files and turn them into the freesurfer MGH files
+%Finally! Here we go to a matlab script. This script will take those asc files and turn them into the freesurfer MGH files.  
+%once you load a surface in freeview, the mgh file can be loaded on top of that. 
 
-%****first you want to add freesurfer and the fsfast toolbox to your matlab path.  these probably were downloaded with your freesurfer installation and will
+%****first you want to add freesurfer and the fsfast toolbox to your matlab path.  These probably were downloaded with your freesurfer installation and will
 %Will change depending on where freesurfer downloaded on your computer
 addpath('.../freesurfer/5.3.0/matlab', '/projects/ics/software/freesurfer/5.3.0/fsfast/toolbox')
 
-%****This overlay will also be in the freesurfer download
+%****This overlay will also be in the freesurfer download, at this point, it doesn't matter which hemisphere you pick. 
 overlay = MRIread('.../lh.thickness.fsaverage.mgh');
 
 %****This is the asc file from script C
@@ -14,6 +15,6 @@ asc = load('Sample2/rGLeftBehavior.asc');
 asc_reshaped = reshape(asc(:,5), overlay.volsize)
 overlay.vol = fast_mat2vol(asc_reshaped, overlay.volsize);
 
-%This code will write out the mgh file you can use in freesurfer! I tend to plot in freeview (personally) by loading the overlay for thickness (the one I used above)
-%and then loading this mgh file on top of it.  That's it and have fun!
+%This code will write out the mgh file you can use in freesurfer! Feel free to change the name of the output file. 
+%That's it and have fun!
 MRIwrite(overlay, 'LogAPLeftICUB.mgh');
